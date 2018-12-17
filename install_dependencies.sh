731709789930fa5b25739837f4c498b4b1adbf58
Zhicheng-Liu <zl@ebi.ac.uk>
2018-12-17 17:01:47 +0000

    Initial commit for libcurl example
    including a simple test suite and the travis, cmake list etc.

---
+++ install_dependencies.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+set -e
+
+if [ "$#" -eq 0 ]
+then
+    OS_NAME="linux"
+elif [ "$#" -eq 1 ]
+then
+  if [[ "$1" == "--help" ]]
+  then
+      echo "$help_install_dependencies"
+      exit
+  elif [[ "$1" == "linux" ]]
+  then
+      OS_NAME="linux"
+  elif [[ "$1" == "osx" ]]
+  then
+      OS_NAME="osx"
+  else
+    echo "$help_install_dependencies"
+    exit
+  fi
+else
+  echo "requires 1 argument at max"
+  echo "$help_install_dependencies"
+  exit
+fi
+
+dependencies_dir=$OS_NAME"_dependencies"
+
+echo "creating directory $dependencies_dir"
+mkdir -p $dependencies_dir && cd $dependencies_dir
+
+
+dependencies_dir_abs_path=`pwd`
+
+echo "installing openssl"
+mkdir openssl
+wget https://www.openssl.org/source/openssl-1.1.1a.tar.gz -O /tmp/openssl-1.1.1a.tar.gz
+tar xzf /tmp/openssl-1.1.1a.tar.gz
+cd openssl-1.1.1a
+LIBS="-lcrypto -ldl" ./config -fPIC no-shared no-threads --prefix=$dependencies_dir_abs_path/openssl --openssldir=$dependencies_dir_abs_path/openssl
+make && make install_sw && cd ..
+
+echo "installing libcurl"
+mkdir curl
+wget https://curl.haxx.se/download/curl-7.62.0.tar.gz -O /tmp/curl-7.62.0.tar.gz
+tar zxf /tmp/curl-7.62.0.tar.gz
+cd curl-7.62.0
+LDFLAGS="-L$dependencies_dir_abs_path/openssl/lib" CPPFLAGS="-I$dependencies_dir_abs_path/openssl/include" LIBS="-lssl -lcrypto -ldl" ./configure --disable-shared --enable-static --without-librtmp --without-ca-bundle --disable-ldap --without-zlib --without-libidn2 --disable-pthreads --disable-threaded-resolver --with-libidn2=$dependencies_dir_abs_path/libidn2 --with-ssl=$dependencies_dir_abs_path/openssl --prefix=$dependencies_dir_abs_path/curl
+make && make install && cd ..
---
+++ test/test_curl.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Zhicheng Liu on 2018-12-17.
+//
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#include <boost/filesystem.hpp>
+
+#include "catch/catch.hpp"
+#include "util/curl_easy.hpp"
+
+TEST_CASE()
+{
+  SECTION("test download")
+  {
+    std::string url = "https://www.ebi.ac.uk/ena/data/view/AB325691&display=fasta";
+    curl::Easy curl;
+    std::stringstream ss;
+    curl.request(ss, url);
+    std::ifstream data{"test/data/AB325691.fa"};
+    std::stringstream sd;
+    while(sd << data.rdbuf());
+    CHECK(ss.str() == sd.str());
+  }
+}
