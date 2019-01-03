6f8731401f4545c66e3c2244723cdf4569cbd329
Zhicheng-Liu <zl@ebi.ac.uk>
2019-01-03 15:14:28 +0000

    With ares

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -42,11 +42,20 @@ cd openssl-1.1.1a
 LIBS="-lcrypto -ldl" ./config -fPIC no-shared no-threads --prefix=$dependencies_dir_abs_path/openssl --openssldir=$dependencies_dir_abs_path/openssl
 make && make install_sw && cd ..
 
+echo "installing c-ares"
+mkdir c-ares
+wget https://c-ares.haxx.se/download/c-ares-1.15.0.tar.gz -O /tmp/c-ares-1.15.0.tar.gz
+tar xzf /tmp/c-ares-1.15.0.tar.gz
+cd c-ares-1.15.0
+./configure --prefix=$dependencies_dir_abs_path/c-ares
+make && make install && cd ..
+
+
 echo "installing libcurl"
 mkdir curl
 wget https://curl.haxx.se/download/curl-7.62.0.tar.gz -O /tmp/curl-7.62.0.tar.gz
 tar zxf /tmp/curl-7.62.0.tar.gz
 cd curl-7.62.0
 #LDFLAGS="-L$dependencies_dir_abs_path/openssl/lib" CPPFLAGS="-I$dependencies_dir_abs_path/openssl/include" LIBS="-lssl -lcrypto -ldl -lpthread" ./configure --disable-shared --enable-static --without-librtmp --without-ca-bundle --disable-ldap --without-zlib --without-libidn2 --enable-pthreads --enable-threaded-resolver --with-ssl=$dependencies_dir_abs_path/openssl --prefix=$dependencies_dir_abs_path/curl
-LDFLAGS="-L$dependencies_dir_abs_path/openssl/lib -L/usr/lib/x86_64-linux-gnu" CPPFLAGS="-I$dependencies_dir_abs_path/openssl/include" LIBS="-lssl -lcrypto -ldl -lpthread" ./configure --disable-shared --enable-static --without-librtmp --without-ca-bundle --disable-ldap --without-zlib --without-libidn2 --enable-pthreads --enable-threaded-resolver --without-nss --with-ssl=$dependencies_dir_abs_path/openssl --prefix=$dependencies_dir_abs_path/curl
+LDFLAGS="-L$dependencies_dir_abs_path/openssl/lib -L/usr/lib/x86_64-linux-gnu" CPPFLAGS="-I$dependencies_dir_abs_path/openssl/include" LIBS="-lssl -lcrypto -ldl" ./configure --disable-shared --enable-static --without-librtmp --without-ca-bundle --disable-ldap --without-zlib --without-libidn2 --without-nss --with-ares=$dependencies_dir_abs_path/c-ares --with-ssl=$dependencies_dir_abs_path/openssl --prefix=$dependencies_dir_abs_path/curl
 make && make install && cd ..
