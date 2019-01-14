a5c16a4d1f1b22e8f487d5ff9edb8343062b5ca1
Zhicheng-Liu <zl@ebi.ac.uk>
2019-01-14 21:25:52 +0000

    build with 14

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -14,19 +14,17 @@ powershell -command "Expand-Archive curl-7.62.0.zip ."
 cd curl-7.62.0
 cd winbuild
 set RTLIBCFG=static
-nmake /f Makefile.vc mode=static VC=15 DEBUG=no RTLIBCFG=static
+nmake /f Makefile.vc mode=static VC=14 DEBUG=no RTLIBCFG=static
 cd ../../
 
 :: Copy headers and libs
 md curl\include
 xcopy /e/i curl-7.62.0\include\* curl\include
 md curl\lib
-xcopy curl-7.62.0\builds\libcurl-vc15-x86-release-static-ipv6-sspi-winssl-obj-lib\libcurl_a.lib curl\lib
+xcopy curl-7.62.0\builds\libcurl-vc14-x86-release-static-ipv6-sspi-winssl-obj-lib\libcurl_a.lib curl\lib
 ren curl\lib\libcurl_a.lib libcurl.lib
 
-dir /s curl-7.62.0\builds
-dir /s curl-7.62.0\winbuild
-dir /s curl
+dumpbin /ARCHIVEMEMBERS curl\lib\libcurl.lib
 
 :: Delete useless files
 del curl-7.62.0.zip
