8ec4a61db1baf2831a7d2c66982a3868a15aec3c
Zhicheng-Liu <zl@ebi.ac.uk>
2019-03-14 11:37:56 +0000

    Add first example with static link

---
+++ install_dependencies.bat
@@ -0,0 +1,29 @@
+@echo off
+
+if not exist windows_dependencies mkdir windows_dependencies
+if exist windows_dependencies/curl-7.62.0-win32-mingw exit /b
+cd windows_dependencies
+
+:: Download zip files
+powershell -command "(new-object System.Net.WebClient).DownloadFile('https://curl.haxx.se/download/curl-7.62.0.zip','curl-7.62.0.zip')"
+
+:: Unzip zip files
+powershell -command "Expand-Archive curl-7.62.0.zip ."
+
+:: Build libcurl
+cd curl-7.62.0/winbuild
+nmake /f Makefile.vc mode=static VC=15 DEBUG=no RTLIBCFG=static
+cd ../../
+
+:: Copy headers and libs
+md curl\include
+xcopy /e/i curl-7.62.0\include\* curl\include
+md curl\lib
+xcopy curl-7.62.0\builds\libcurl-vc15-x86-release-static-ipv6-sspi-winssl-obj-lib\libcurl_a.lib curl\lib
+ren curl\lib\libcurl_a.lib libcurl.lib
+
+:: Delete useless files
+del curl-7.62.0.zip
+
+dir
+cd ..
---
+++ test/test_curl.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Zhicheng Liu on 2018-12-17.
+//
+#include <fstream>
+#include <limits>
+#include <sstream>
+#include <string>
+
+#include "catch/catch.hpp"
+#include "util/curl_easy.hpp"
+
+TEST_CASE()
+{
+  SECTION("test download")
+  {
+    std::cout << std::numeric_limits<std::streamsize>::max() << std::endl;
+    std::string url = "https://www.ebi.ac.uk/ena/data/view/AB325691&display=fasta";
+    curl::Easy curl;
+    std::stringstream ss;
+    curl.request(ss, url);
+    std::ifstream data{"test/data/AB325691.fa"};
+    std::stringstream sd;
+    while(sd << data.rdbuf());
+    CHECK(ss.str() == sd.str());
+  }
+}
